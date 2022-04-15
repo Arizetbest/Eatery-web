ee9e06e8bfc7f8a1b604c76ff0a224ebe3befff3
Arize Onubiyi <arizetbest@gmail.com>
2022-04-15 01:01:45 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#FF6363',
+        secondary:{
+          100:'#E2E2D5',
+          200:'#888883',
+          300:'999663',
+        }
+      },
+      fontFamily:{
+        body:['My Soul']
+      }
+    },
+  },
+  plugins: [],
+}
+ 
